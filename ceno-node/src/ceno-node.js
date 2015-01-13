549cfe53d7436bbd321a61cd7f37741c00efa0a6
Zack Mullaly <redwire@riseup.net>
2015-01-13 20:26:27 -0330

    Wrote some code to outline the interaction between ceno node and whatever cache medium is being used

--- ceno-node/src/ceno-node.js
+++ ceno-node/src/ceno-node.js
@@ -8,7 +8,54 @@ var http = require('http');
 // Likewise we could initialize the cache to use freenet
 var cache = require('../lib/cache').freenet('localhost:9001');
 
+// Temporarily treat the bundler like it doesn't do anything
+var bundler = {
+  makeBundle: function (url, callback) {
+    callback(null, url);
+  }
+};
+
+/* When a new bundle is created for a page that hasn't been cached already,
+ * we want to cache it in whatever media we are using.
+ */
+function cacheNewBundle(cacheObj, bundleID, bundle) {
+  cacheObj.write({
+    id: bundleID,
+    bundle: bundle
+  }, function (err, response) {
+    console.log('Response from cache write : ' + response);
+  });
+}
+
+/* We expect that the bundler cache will provide `response` data that contains:
+ * 1. {bundleFound: true, bundle: <bundle-string>} if a bundle exists
+ * 2. {bundleFound: false, bundleID: <unique-id>} if not
+ */
+function requestHandler(req, res) {
+  cache.read(req.url, function (err, response) {
+    if (err) {
+      // Do something special if reading from the cache is broken
+    } else if (!response.bundleFound) {
+      bundler.makeBundle(req.url, function (err, bundle) {
+        if (err) {
+          // Do something in case the bundler fails
+        } else {
+          res.write(bundle);
+          res.end();
+          cacheNewBundle(cache, response.bundleID, bundle);
+        } 
+      });
+    } else {
+      res.write(bundle);
+      res.end();
+    }
+  });
+}
+
+
 http.createServer(function (req, res) {
+  // The following two calls demonstrate that, regardless of how we initialize our
+  // cache object, the simple interface it provides can be used uniformly.
   cache.read('www.google.com', function (err, data) {
     console.log('Received data from local cache.');
     console.log(data);
