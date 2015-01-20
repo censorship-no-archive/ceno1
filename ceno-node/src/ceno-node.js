38e8f3df748bc0d1360d7d188a446d9cfb3e2254
Zack Mullaly <redwire@riseup.net>
2015-01-19 20:49:41 -0330

    Outline functions for handlers CeNo-node needs

--- ceno-node/src/ceno-node.js
+++ ceno-node/src/ceno-node.js
@@ -6,6 +6,10 @@ var querystring = require('querystring');
 var cache = require('../lib/cache').local();
 var bundler = require('../lib/bundler');
 
+var portNumber = 3090;
+var address = '127.0.0.1';
+var processes = {};
+
 /* When a new bundle is created for a page that hasn't been cached already,
  * we want to cache it in whatever media we are using.
  */
@@ -19,12 +23,12 @@ function cacheNewBundle(cacheObj, url, bundleID, bundle) {
   });
 }
 
-/* We expect that the bundler cache will provide `response` data that contains:
+/* Query the Cache Server for a bundled page.
+ * We expect that the bundler cache will provide `response` data that contains:
  * 1. {bundleFound: true, bundle: <bundle-string>} if a bundle exists
  * 2. {bundleFound: false, bundleID: <unique-id>} if not
  */
-function requestHandler(req, res) {
-  console.log('req.url = ' + req.url);
+function requestBundle(req, res) {
   var url = querystring.parse(urllib.parse(req.url).query).url;
   console.log('Got request for ' + url);
   cache.read(url, function (err, response) {
@@ -49,5 +53,31 @@ function requestHandler(req, res) {
   });
 }
 
-http.createServer(requestHandler).listen(3090, '127.0.0.1');
+/* Request that the Transport begin making a new bundle.
+ */
+function makeNewBundle(url) {
 
+}
+
+/* Handle requests from the user to have a bundled page fetched.
+ */
+function handleBundleRequest(req, res) {
+
+}
+
+/* Handle requests from the Transport server informing us that
+ * a bundling process has been completed.
+ */
+function handleProcessCompletion(req, res) {
+
+}
+
+/* Route requests.
+ */
+function requestHandler(req, res) {
+  console.log('req.url = ' + req.url);
+  // For now, let's just route requests as before.
+  requestBundle(req, res);
+}
+
+http.createServer(requestHandler).listen(portNumber, address);
