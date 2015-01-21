58233dd09bdf06f312fa4caa488db0ef405e1125
Zack Mullaly <redwire@riseup.net>
2015-01-21 19:29:08 -0330

    Wrote tests for cache server that run successfully

--- ceno-node/test/testcacheserver.js
+++ ceno-node/test/testcacheserver.js
@@ -1,26 +1,27 @@
 var http = require('http');
+var assert = require('assert');
 var request = require('superagent');
-var cache = require('./cacheserver');
+//var cache = require('./cacheserver');
 var testServer = require('./testserver');
 
-var reader = http.createServer(cache.reader);
-var writer = http.createServer(cache.writer);
+//var reader = http.createServer(cache.reader);
+//var writer = http.createServer(cache.writer);
 
-var readerAddress = 'localhost:' + cache.readerPort;
-var writerAddress = 'localhost:' + cache.writerPort;
+var readerAddress = 'localhost:3091';
+var writerAddress = 'localhost:3092';
 
 var testURL = 'http://nowhere.place/just/testing/';
 var testBundleData = 'HELLOWORLD';
 
-reader.listen(cache.readerPort);
-writer.listen(cache.writerPort);
+//reader.listen(cache.readerPort);
+//writer.listen(cache.writerPort);
 
-testServer(reader, [testCacheRead]);
-testServer(writer, [testCacheWrite]);
+//testServer(reader, [testCacheRead]);
+//testServer(writer, [testCacheWrite]);
 
 function testCacheRead(done) {
   request
-  .get(testURL)
+  .get(readerAddress + '/?url=' + testURL)
   .end(function (err, response) {
     if (err) {
       console.log('Test resulted in error.');
@@ -28,10 +29,10 @@ function testCacheRead(done) {
     } else {
       console.log('Test resulted in success.');
       var json = JSON.parse(response.text);
-      assert json.hasOwnPropert('bundleFound');
+      assert.ok(json.hasOwnProperty('bundleFound'), 'Response does not contain bundleFound field.');
       if (json.bundleFound) {
-        assert json.hasOwnProperty('bundle');
-        assert json.bundle === testBundleData;
+        assert.ok(json.hasOwnProperty('bundle'), 'Successful bundle fetch has no bundle field.');
+        assert.strictEqual(json.bundle, testBundleData, 'Did not fetch correct bundle data.');
       }
     }
   });
@@ -48,14 +49,17 @@ function testCacheWrite(done) {
       console.log(err);
     } else {
       console.log('Test resulted in success.');
-      var json = JSON.parse(response.text);
-      assert json.hasOwnProperty('error');
-      assert json.hasOwnProperty('status');
+      var json = response.body;
+      assert.ok(json.hasOwnProperty('error'), 'Response does not contain error field.');
+      assert.ok(json.hasOwnProperty('status'), 'Response does not contain status field.');
       if (json.status === 'failure') {
-        assert json.error.hasOwnProperty('message');
+        assert.ok(json.error.hasOwnProperty('message'), 'Failed response error has no message field.');
       }
     }
   });
   done();
 }
 
+var nop = function() {};
+testCacheRead(nop);
+testCacheWrite(nop);
