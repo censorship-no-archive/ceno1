c61bffff43fdec118f837a043f5d2f3c4e34ba2d
Marios Isaakidis <misaakidis@yahoo.gr>
2015-01-21 17:24:46 +0200

    CeNo listening on two ports and using different handlers for each of them; Added handler stub for the /store handler; Now using ContextHandlers and VirtualHosts for the embedded jetty

--- plugin-ceno/src/plugins/CeNo/CeNo.java
+++ plugin-ceno/src/plugins/CeNo/CeNo.java
@@ -4,20 +4,20 @@ import freenet.pluginmanager.*;
 import freenet.support.Logger;
 
 import org.eclipse.jetty.server.Server;
-
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import plugins.CeNo.FreenetInterface.NodeInterface;
-
-import java.util.Date;
 
 
 public class CeNo implements FredPlugin {
 
     //private final static Logger LOGGER = Logger.getLogger(CeNo.class.getName());
     private PluginRespirator pluginRespirator;
-	private volatile boolean goon = true;
 
     //Need to be read from config
     private final static Integer ceNoPluginHttpPort = 3091;
+    private final static Integer ceNoPluginCachePort = 3092;
 
     private Server ceNoHttpServer;
     
@@ -36,15 +36,34 @@ public class CeNo implements FredPlugin {
 
         ceNoHttpServer = new Server(ceNoPluginHttpPort);
         
-        CeNoHttpHandler handler = new CeNoHttpHandler();
-        ceNoHttpServer.setHandler(handler);
+        ServerConnector httpConnector = new ServerConnector(ceNoHttpServer);
+        httpConnector.setName("http");
+        httpConnector.setPort(ceNoPluginHttpPort);
+        ServerConnector cacheConnector = new ServerConnector(ceNoHttpServer);
+        cacheConnector.setName("cache");
+        cacheConnector.setPort(ceNoPluginCachePort);
+        
+        ceNoHttpServer.setConnectors(new ServerConnector[]{httpConnector, cacheConnector});
+
+        ContextHandlerCollection handlers = new ContextHandlerCollection();
+        ceNoHttpServer.setHandler(handlers);
+        
+        ContextHandler httpCtxHandler = new ContextHandler();
+        httpCtxHandler.setHandler(new CeNoHttpHandler());
+        httpCtxHandler.setVirtualHosts(new String[]{"@http"});
+        ContextHandler cacheCtxHandler = new ContextHandler();
+        cacheCtxHandler.setHandler(new CeNoCacheHanlder());
+        cacheCtxHandler.setVirtualHosts(new String[]{"@cache"});
+        
+        
+        handlers.addHandler(httpCtxHandler);
+        handlers.addHandler(cacheCtxHandler);
 
         try {
         	ceNoHttpServer.start();
         	ceNoHttpServer.join();
         } catch (Exception e) {
             e.printStackTrace();
-        
         }
     }
 
---
+++ plugin-ceno/src/plugins/CeNo/CeNoCacheHanlder.java
@@ -0,0 +1,33 @@
+package plugins.CeNo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+/* ------------------------------------------------------------ */
+/** CeNo Plugin handler for requests to cache bundles
+ * 	
+ * CeNoCacheHanlder listens to :{@link CeNo#ceNoPluginCachePort} port
+ * and under the "/store" route for POST requests.
+ * Those POST requests include the bundled page as well as the original url.
+ * The handler caches the given bundle under its signed subspace.
+ * Upon successful insertion, the handler replies with "stored".
+ */
+public class CeNoCacheHanlder extends AbstractHandler {
+
+	public void handle(String target,Request baseRequest,HttpServletRequest request,HttpServletResponse response)
+			throws IOException, ServletException
+	{ 
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println("stored");
+		baseRequest.setHandled(true);
+	}
+
+}
