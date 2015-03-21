57eb5a536c16aef22d61e2dbe4bea01f223b9342
Zack Mullaly <redwire@riseup.net>
2015-03-21 00:27:46 -0230

    Initial implementation of new protocol for client side of things

--- ceno-node/src/client.go
+++ ceno-node/src/client.go
@@ -5,24 +5,24 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/url"
 	"bufio"
 	"io/ioutil"
 	"strings"
 	"bytes"
 	"encoding/json"
 	"path"
+	"time"
 )
 
-type Process struct {
-	Clients map[string]bool
-	Bundle  []byte
-	URL     string
-}
+// A process is just a url that is being waited on.
+type Process string
 
-type Request struct {
-	URL    string
-	Output chan []byte
-	Source string
+// Result of a bundle lookup from cache server.
+type Result struct {
+	Ready  bool
+	Found  bool
+	Bundle []byte
 }
 
 // Configuration struct to be replaced by a decoded JSON file's contents.
@@ -39,175 +39,60 @@ func pleaseWait(url string) []byte {
 	return bytes.Replace(content, []byte("{{REDIRECT}}"), []byte(url), 1)
 }
 
-// Have the Transport Server create a new bundle
-func requestNewBundle(url string, reportCompletion chan Process) bool {
-	remoteAddr, _ := net.ResolveTCPAddr("tcp", Configuration.BridgeServer)
-	conn, err := net.DialTCP("tcp", nil, remoteAddr)
-	readyMSG := []byte("READY\n")
-	errorMSG := []byte("ERROR expected message COMPLETE\n")
-	if err != nil {
-		fmt.Println("Could not establish connection to bridge server at " + Configuration.BridgeServer)
-		return false // Failed to get new bundle
+func checkOnLookup(lookupURL string) Result {
+	// The cache server will keep track of running lookups so we will just
+	// ask for a URL the usual way until it responds saying a lookup is complete.
+	response, err := http.Get(config.EdgeServer + "?url=" + url.QueryEscape(lookupURL))
+	defer response.Body.Close()
+	if err != nil || response.StatusCode != 200 {
+		return Result { false, false, nil }
 	}
-	reader := bufio.NewReader(conn)
-	conn.Write([]byte("BUNDLE " + url + "\n"))
-	result, _ := reader.ReadString('\n')
-	if !strings.HasPrefix(result, "COMPLETE") {
-		if strings.HasPrefix(result, "ERROR") {
-			fmt.Println("!!! Got error message " + result[strings.Index(result, " "):])
-			// Report process completion so that requests do not sit around in memory.
-			// Serve the error as the bundle so it reaches the user.
-			reportCompletion <- Process { Clients: nil, Bundle: []byte(result), URL: url }
-		} else {
-			fmt.Println("Bridge server not adhering to protocol.")
-			fmt.Println("In response to BUNDLE, sent " + result)
-			conn.Write(errorMSG)
-			// Report process completion so that requests do not sit around in memory.
-			reportCompletion <- Process { Clients: nil, Bundle: errorMSG, URL: url }
-		}
-		conn.Close()
-		return false
+	decoder := json.NewDecoder(response.Body)
+	var result Result
+	if err := decoder.Decode(&result); err == io.EOF {
+		return Result { false, false, nil }
 	}
-	conn.Write(readyMSG)
-	bundle, _ := ioutil.ReadAll(reader)
-	reportCompletion <- Process { Clients: nil, Bundle: bundle, URL: url }
-	conn.Close()
-	return true
+	return result
 }
 
-// Check if a bundle has already been cached and, if so, write it to the ResponseWriter
-func readFromCache(url string, reportCompletion chan Process) bool {
-	remoteAddr, _ := net.ResolveTCPAddr("tcp", Configuration.EdgeServer)
-	conn, err := net.DialTCP("tcp", nil, remoteAddr)
-	okayMSG := []byte("OKAY\n")
-	readyMSG := []byte("READY\n")
-	errorMSG := []byte("ERROR expected message RESULT (not) found\n")
-	if err != nil {
-		fmt.Println("Could not establish connection to edge server at " + Configuration.BridgeServer)
-		return false // Failed to lookup
-	}
-	reader := bufio.NewReader(conn)
-	conn.Write([]byte("LOOKUP " + url + "\n"))
-	result, _ := reader.ReadString('\n')
-	if !strings.HasPrefix(result, "RESULT") {
-		if strings.HasPrefix(result, "ERROR") {
-			fmt.Print("!!! Got error message " + result[strings.Index(result, " ") + 1:])
-		} else {
-			fmt.Println("Edge server not adhering to protocol.")
-			fmt.Println("In response to LOOKUP, sent " + result)
-			conn.Write(errorMSG)
-		}
-		conn.Close()
-		return false
-	}
-	if strings.HasSuffix(result, "not found\n") {
-		fmt.Println("No bundle found in cache")
-		conn.Write(okayMSG)
-		conn.Close()
-		return false
-	} else if strings.HasSuffix(result, "found\n") {
-		conn.Write(readyMSG)
-		bundle, _ := ioutil.ReadAll(reader)
-		if (bytes.HasPrefix(bundle, []byte("ERROR"))) {
-			error := string(bundle)
-			fmt.Println("!!! Got error instead of bundle: " + error[strings.Index(error, " "):])
-			conn.Close()
-			return false
-		} else {
-			reportCompletion <- Process { Clients: nil, Bundle: bundle, URL: url }
-			conn.Close()
-			return true
-		}
-	}
-	fmt.Println("Unrecognized RESULT status received from edge server")
-	conn.Write(errorMSG)
-	conn.Close()
-	return false
-}
-
-func getBundle(url string, reportCompletion chan Process) {
-	foundInCache := readFromCache(url, reportCompletion)
-	if !foundInCache {
-		fmt.Println("Did not find a bundle for " + url + " in cache")
-		producedBundle := requestNewBundle(url, reportCompletion)
-		if !producedBundle {
-			fmt.Println("Error; Could not produce new bundle")
-		} else {
-			fmt.Println("Created a new bundle for " + url)
-		}
-	} else {
-		fmt.Println("Found bundle for " + url + " in cache")
+func makeNewBundle(url string) {
+	// POST to the bridge server to have it start making a new bundle.
+	// We can ignore the content of the response since it is not used.
+	response, err := http.Post(config.BridgeServer, "text/plain", strings.Reader(url))
+	defer response.Body.Close()
+	if err != nil || response.StatusCode != 200 {
+		fmt.Println("Got error POSTing to bridge server")
+		fmt.Println(err)
 	}
 }
 
-func issueBundles(requests chan Request) {
-	processes := make(map[string]Process)
-	// Have the cache lookup and transport communicator routines commuicate when
-	// new bundles have been found or produced through the this channel
-	finishedProcesses := make(chan Process)
-	for {
-		select {
-		case request := <-requests:
-			_, exists := processes[request.URL]
-			if exists { // We have already started processing a lookup or bundling for this URL
-				fmt.Println("Existing process to bundle " + request.URL + " found")
-				if len(processes[request.URL].Bundle) > 0 { //The bundle has been prepared
-					request.Output <- processes[request.URL].Bundle
-					_, hasRequested := processes[request.URL].Clients[request.Source]
-					if hasRequested {
-						// Remove the source of the request from the set of clients
-						// so we can get closer to removing the bundle from memory
-						delete(processes[request.URL].Clients, request.Source)
-						fmt.Println("Removed " + request.Source + " as a client")
-					}
-					if len(processes[request.URL].Clients) == 0 {
-						// Remove the process from memory since no one is waiting on it
-						fmt.Println("Removing process for " + request.URL)
-						delete(processes, request.URL)
-					}
+func makeProxyHandler() func (http.ResponseWriter, *http.Request) {
+	// Maintain a map of "processes" (URLs being looked up by cache) for fast access
+	var lookups map[Process]bool = make(map[Process]bool)
+	return func (w http.ResponseWriter, r *http.Request) {
+		url := r.URL.String()
+		_, processExists := lookups[url]
+		if processExists {
+			// Rather than maintaining a timer that will periodically check if the bundle
+			// is ready, we will just ask the cache server if it is finished the lookup
+			// when a request comes back in
+			result := checkOnLookup(url)
+			if result.Ready {
+				if result.Found {
+					delete lookups[url]
+					w.Write(result.Bundle)
 				} else {
-					fmt.Println("Serving please wait page to " + request.Source)
-					request.Output <- pleaseWait(request.URL)
-					_, hasRequested := processes[request.URL].Clients[request.Source]
-					if !hasRequested {
-						// Add the new source to the set of clients waiting for the bundle
-						processes[request.URL].Clients[request.Source] = true
-					}
+					go makeNewBundle(url)
+					w.Write(pleaseWait(url))
 				}
-			} else {
-				// Start a new process for the requested URL's bundle
-				fmt.Println("Creating a new process to get the bundle for " + request.URL)
-				firstClient := make(map[string]bool)
-				firstClient[request.Source] = true
-				processes[request.URL] = Process { Clients: firstClient, Bundle: []byte(""), URL: request.URL }
-				go getBundle(request.URL, finishedProcesses)
-				request.Output <- pleaseWait(request.URL)
+				delete lookups[url]
 			}
-		case finished := <-finishedProcesses:
-			// When either a successful cache lookup completes or a new bundle is produced,
-			// store the bundle's contents in an existing process to be fetched by clients
-			fmt.Println("Finished retrieving a bundle for " + finished.URL)
-			_, stillWorking := processes[finished.URL]
-			if stillWorking {
-				// Cannot assign directly to a map's struct value so use this workaround
-				tempProc := processes[finished.URL]
-				tempProc.Bundle = finished.Bundle
-				processes[finished.URL] = tempProc
-			}
+		} else {
+			lookups[url] = true
+			// Start a lookup process but ignore the result since a bundle can't be ready
+			checkOnLookup(url)
+			w.Write(pleaseWait(url))
 		}
-	}
-}
-
-func makeProxyHandler(toDealer chan Request) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Leave it to the bundle dealer to serve the bundle or please wait page.
-		// This avoids duplicating bundles in memory through a channel.
-		fmt.Println("Requesting bundle for " + r.RequestURI + " on behalf of " + r.RemoteAddr)
-		out := make(chan []byte)
-		host, _, _ := net.SplitHostPort(r.RemoteAddr)
-		toDealer <- Request { r.RequestURI, out, host }
-		page := <-out
-		w.Write(page)
 	}
 }
 
@@ -222,10 +107,7 @@ func main() {
 		fmt.Println("Could not read configuration file at " + configPath + "\nExiting.")
 		return
 	}
-	toBundleDealer := make(chan Request)
-	// Read requests for bundles for given URLs from toBundleDealer, serve through frBundleDealer
-	go issueBundles(toBundleDealer)
-	http.HandleFunc("/", makeProxyHandler(toBundleDealer))
+	http.HandleFunc("/", makeProxyHandler())
 	fmt.Println("CeNo proxy server listening at http://localhost" + Configuration.PortNumber)
 	http.ListenAndServe(Configuration.PortNumber, nil)
 }
