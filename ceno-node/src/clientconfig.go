897a2f50b3bf3bcba03a3a6319b9a54bae960850
Zack Mullaly <redwire@riseup.net>
2015-04-20 13:47:45 -0230

    Added config validators and a function to read config details from stdin

--- ceno-node/src/clientconfig.go
+++ ceno-node/src/clientconfig.go
@@ -3,6 +3,9 @@ package main
 import (
 	"os"
 	"encoding/json"
+	"strconv"
+	"net/url"
+	"fmt"
 )
 
 // Configuration struct containing fields required by client to run proxy server
@@ -32,8 +35,47 @@ var DefaultConfiguration Config = Config {
 	":3090",
 	"http://localhost:3091/lookup",
 	"http://localhost:3093/create",
-	"The page you requested could not be fetched at this time.\nPlease try again in a moment.\n",
+	"Page not found",
 	"views/wait.html"
+}
+
+// Functions to verify that each configuration field is well formed.
+
+// Port numbers for the proxy server to run on must be specified in the
+// form ":<number>".
+func validPortNumber(port string) bool {
+	colonPrefixSupplied = port[0] == ':'
+	number, parseErr := strconv.Atoi(port[1:])
+	if parseErr != nil {
+		return false
+	}
+	return colonPrefixSupplied && number > 0 && number <= 65535
+}
+
+// Rather than trying to limit the format of the URL for a server and
+// restricting our use cases, we will broadly say that any URL that
+// parses correctly is valid.
+func validCacheServer(cacheServer string) bool {
+	_, err := url.Parse(cacheServer)
+	return err == nil
+}
+
+// Likewise for the request server, we only test that the URL can be parsed.
+func validRequestServer(requestServer string) bool {
+	_, err := url.Parse(requestServer)
+	return err == nil
+}
+
+// No expectations are made with regards to the error message provided.
+func validErrorMessage(errMsg string) bool {
+	return true
+}
+
+// We don't want to do too much work verifying that the content of the
+// provided page is valid HTML so we will settle for ensuring the file exists.
+func validPleaseWaitPage(location string) bool {
+	_, err := os.Stat(location)
+	return os.IsExist(err)
 }
 
 // Try to read a configuration in from a file.
@@ -49,4 +91,58 @@ func ReadConfigFile(fileName string) (Config, error) {
 		return nil, err
 	}
 	return configuration, nil
+}
+
+// Read configuration information from stdin
+func GetConfigFromUser() Config {
+	var configuration Config
+	// We will accept an error message once at the end
+	vPort, vCS, vRS, vPWPage := false, false, false, false
+	done := false
+	fmt.Println("Please provide new settings using the format of the examples provided to configure CeNo.")
+	fmt.Println("You can press enter/return without entering anything else to use the default.")
+	for !done {
+		vPort := validPortNumber(configuration.PortNumber)
+		if !vPort {
+			fmt.Print("Proxy server port number [" + DefaultConfiguration.PortNumber + "]: ")
+			fmt.Scanln(&configuration.PortNumber)
+			if len(configuration.PortNumber) == 0 {
+				configuration.PortNumber = DefaultConfiguration.PortNumber
+			}
+		}
+		vCS := validCacheServer(configuration.CacheServer)
+		if !vCS {
+			fmt.Print("Local cache server lookup URL [" + DefaultConfiguration.CacheServer + "]: ")
+			fmt.Scanln(&configuration.CacheServer)
+			if len(configuration.CacheServer) == 0 {
+				configuration.CacheServer = DefaultConfiguration.CacheServer
+			}
+		}
+		vRS := validRequestServer(configuration.RequestServer)
+		if !vRS {
+			fmt.Print("Bundle creation request server URL [" + DefaultConfiguration.RequestServer + "]: ")
+			fmt.Scanln(&configuration.RequestServer)
+			if len(configuration.RequestServer) == 0 {
+				configuration.RequestServer = DefaultConfiguration.RequestServer
+			}
+		}
+		vPWPage := validPleaseWaitPage(configuration.PleaseWaitPage)
+		if !vPWPage {
+			fmt.Print("Path to please wait page [" + DefaultConfiguration.PleaseWaitPage + "]: ")
+			fmt.Scanln(&configuration.PleaseWaitPage)
+			if len(configuration.PleaseWaitPage) == 0 {
+				configuration.PleaseWaitPage = DefaultConfiguration.PleaseWaitPage
+			}
+		}
+		done = vPort && vCS && vRS && vPWPage
+		if !done {
+			fmt.Println("Some data was entered incorrectly. Please try again.")
+		}
+	}
+	fmt.Print("Error message for undiscovered pages [" + DefaultConfiguration.ErrorMsg + "]: ")
+	fmt.Scanln(&configuration.ErrorMsg)
+	if len(configuration.ErrorMsg) == 0 {
+		configuration.ErrorMsg = DefaultConfiguration.ErrorMsg
+	}
+	return configuration
 }
