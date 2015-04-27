a65705726f0e48f134d6d44f624df7f9e3181f48
Zack Mullaly <redwire@riseup.net>
2015-04-27 12:18:47 -0230

    Moving the HTTP request handler for the bundle server into a module to ease testing and deployment

---
+++ ceno-node/src/bundle-server.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var http = require('http');
+
+var bsHandlerMaker = require('bshandler');
+
+const configFile = path.join('..', 'config', 'node.json');
+
+var defaultConfig = {
+  port: 3091,
+  requestReceiver: 'http://localhost:3092'
+};
+
+var config = defaultConfig;
+
+try {
+  config = JSON.parse(fs.readFileSync(configFile));
+} catch (ex) {
+  console.log('Could not load config; Error: ' + ex.message);
+  config = defaultConfig;
+  console.log('Using default config.');
+}
+
+var bsHandler = bsHandlerMaker(config);
+
+var server = http.createServer(bsHandler);
+server.listen(config.port);
+console.log('Running bundler server on port ' + config.port);
