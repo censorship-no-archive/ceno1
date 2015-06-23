96cd3b8e86b5a019edd87567ae9dbafe1870de17
Zack Mullaly <redwire@riseup.net>
2015-06-23 13:01:52 -0400

    Move the logic for (de)activating CeNo to the background script and switch to using a toggle button

--- ceno-chrome/background.js
+++ ceno-chrome/background.js
@@ -1,9 +1,41 @@
+/* If the URL has the https scheme, make it http so that CeNo client
+ * can actually handle it for us.
+ *
+ * I would call this extension HTTPS Nowhere, but EFF might be mad.
+ *
+ * @param {string} url - The URL being requested, must not contain extraneous characters
+ */
+function stripHTTPS(url) {
+  if (url.match('^https') !== null) {
+    return url.replace('https', 'http');
+  }
+  return url;
+}
+
+/* Intercept all incoming requests.
+ * Note that synchronous XMLHttpRequests don't invoke blocking handlers.
+ * It is thus necessary not to block requests.
+ *
+ * We may want to attach several listeners here to deal with different
+ * types of requests differently.
+ *
+ * @param {object} details - Information about the request
+ */
+function sendToProxy(details) {
+  if (details.method.toLowerCase() !== 'get') {
+    // Do something special with POST/PUT/... requests?
+  }
+  var url = stripHTTPS(details.url);
+  console.log('Stipped url: ' + url);
+  return { redirectUrl: url };
+}
+
 /* Add listeners to the event fired when a site is requested or a
  * redirect is issued to use the CeNo proxy.
  */
-function activateCeNo(callback) {
+function activateCeNo() {
   chrome.webRequest.onBeforeRequest.addListener(
-    callback, {urls: ['https://*/*', 'http://*/*']}, ['blocking']);
+    sendToProxy, {urls: ['https://*/*', 'http://*/*']}, ['blocking']);
   //chrome.webRequest.onBeforeRedirect.addListener(
   //  sendToProxy, {urls: ['https://*/*', 'http://*/*']});
   document.getElementById('activeState').value = 'true';
@@ -13,9 +45,41 @@ function activateCeNo(callback) {
 /* Remove listeners to the event fired when a site is requested or a
  * redirect is issued to use the CeNo proxy.
  */
-function deactivateCeNo(callback) {
-  chrome.webRequest.onBeforeRequest.removeListener(callback);
+function deactivateCeNo() {
+  chrome.webRequest.onBeforeRequest.removeListener(sendToProxy);
   //chrome.webRequest.onBeforeRedirect.removeListener(sendToProxy);
   document.getElementById('activeState').value = 'false';
   console.log('Deactivated CeNo');
 }
+
+/* Check whether the extension is active by reading the state of
+ * the hidden input in the background page.
+ *
+ * @param {function(boolean)} callback - A callback to be invoked with the active status of the plugin
+ */
+function isActive(callback) {
+  var input = document.getElementById('activeState');
+  callback(input.value === 'true');
+}
+
+/* Listen for messages from the popup script informing us that
+ * a toggle button was clicked.
+ */
+chrome.extension.onMessage.addListener(function (req, sender, respond) {
+  switch (req.directive) {
+  case 'icon-clicked':
+    isActive(function (active) {
+      console.log('Active: ' + active.toString());
+      if (active) {
+        deactivateCeNo();
+        respond({ statusActive: false });
+      } else {
+        activateCeNo();
+        respond({ statusActive: true });
+      }
+    });
+    break;
+  default:
+    console.log('Unrecognized directive');
+  };
+});
--- ceno-chrome/intercept.js
+++ ceno-chrome/intercept.js
@@ -2,74 +2,25 @@
 var cenoPort = 3090;
 var cenoAddr = '127.0.0.1';
 
-/* Render a status message in the popup displayed by the plugin.
- * @param {string} statusText - The status message to display
- */
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-  console.log('Set status text');
-}
-
-/* Attach event handlers to UI elements
+/* Attach event handlers to UI elements.
+ * When the toggle button in the popup is clicked, we want to
+ * notify the background script of the event and display a
+ * message to the user to inform them what state CeNo has been
+ * put in.
  */
 document.addEventListener('DOMContentLoaded', function() {
-  // TODO - Remember the state CeNo was in.
-  // Default state for CeNo is off.
-  renderStatus('Your browser is will not proxy requests to CeNo');
-  activateBox = document.getElementById('useCeno');
-  activateBox.addEventListener('click', function (evt) {
-    console.log('Checkbox click event fired');
-    var bg = chrome.extension.getBackgroundPage();
-    if (activateBox.checked) {
-      renderStatus('Your browser will proxy requests to CeNo ' +
-        'through http://' + cenoAddr + ':' + cenoPort);
-      bg.activateCeNo(sendToProxy);
-    } else {
-      renderStatus('Your browser is will not proxy requests to CeNo');
-      bg.deactivateCeNo(sendToProxy);
-    }
+  var toggleBtn = document.getElementById('useCeno');
+  toggleBtn.addEventListener('click', function (evt) {
+    console.log('Button click event fired');
+    chrome.extension.sendMessage({
+      directive: 'icon-clicked',
+    }, function (response) {
+      if (response.statusActive) {
+        alert('CeNo has been activated!');
+      } else {
+        alert('CeNo has been deactivated.');
+      }
+      this.close();
+    });
   });
 });
-
-/* If the URL has the https scheme, make it http so that CeNo client
- * can actually handle it for us.
- *
- * I would call this extension HTTPS Nowhere, but EFF might be mad.
- *
- * @param {string} url - The URL being requested, must not contain extraneous characters
- */
-function stripHTTPS(url) {
-  if (url.match('^https') !== null) {
-    return url.replace('https', 'http');
-  }
-  return url;
-}
-
-/* Intercept all incoming requests.
- * Note that synchronous XMLHttpRequests don't invoke blocking handlers.
- * It is thus necessary not to block requests.
- *
- * We may want to attach several listeners here to deal with different
- * types of requests differently.
- *
- * @param {object} details - Information about the request
- */
-function sendToProxy(details) {
-  if (details.method.toLowerCase() !== 'get') {
-    // Do something special with POST/PUT/... requests?
-  }
-  var url = stripHTTPS(details.url);
-  console.log('Stipped url: ' + url);
-  return { redirectUrl: url };
-  /*
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      // Set the content of the page? Must use a content script.
-    }
-  };
-  xhr.send(null);
-  */
-}
-
