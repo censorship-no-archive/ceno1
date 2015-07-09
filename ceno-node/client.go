4589cb006e4dd15554b6d765fb99314ad9be7299
Zack Mullaly <redwire@riseup.net>
2015-07-09 16:56:16 -0400

    Finished preparing the rest of errorhandling.go for translation

--- ceno-node/client.go
+++ ceno-node/client.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/nicksnyder/go-i18n/i18n"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
 	"regexp"
 )
 
@@ -17,21 +20,14 @@ var Configuration Config
 // Verifies a URL as valid (enough)
 const URL_REGEX = "(https?://)?(www\\.)?\\w+\\.\\w+"
 
-// In the case that wait.html cannot be served, we will respond with a
-// plain text message to the user.
-const PLEASE_WAIT_PLAINTEXT = /* Multi-line strings, yeah! */ `
-The page you have requested is being prepared.
-Plesae refresh this page in a few seconds to check if it is ready.
-`
-
 // The header used to communicate from the browser extension to the bundle server
 // that a request for http://site.com was rewritten from one for https://site.com.
 const REWRITTEN_HEADER = "X-Ceno-Rewritten"
 
 // Result of a bundle lookup from cache server.
 type Result struct {
-  ErrCode  ErrorCode
-  ErrMsg   string
+	ErrCode  ErrorCode
+	ErrMsg   string
 	Complete bool
 	Found    bool
 	Bundle   string
@@ -43,8 +39,8 @@ type Result struct {
 // by pages, browser plugins, and so on to check if CeNo client
 // is running.
 func WriteProxyHeader(w http.ResponseWriter) http.ResponseWriter {
-  w.Header().Add("X-Ceno-Proxy", "yxorP-oneC-X")
-  return w
+	w.Header().Add("X-Ceno-Proxy", "yxorP-oneC-X")
+	return w
 }
 
 // Serve a page to inform the user that a bundle for the site they requested is
@@ -54,7 +50,8 @@ func WriteProxyHeader(w http.ResponseWriter) http.ResponseWriter {
 func pleaseWait(url string) ([]byte, bool) {
 	content, err := ioutil.ReadFile(Configuration.PleaseWaitPage)
 	if err != nil {
-		return []byte(PLEASE_WAIT_PLAINTEXT), false
+		T, _ := i18n.Tfunc(os.Getenv("LANGUAGE"), "en-us")
+		return []byte(T("please_wait_txt")), false
 	} else {
 		return bytes.Replace(content, []byte("{{REDIRECT}}"), []byte(url), 1), true
 	}
@@ -64,7 +61,7 @@ func pleaseWait(url string) ([]byte, bool) {
 // The LCS is expected to respond to this request with just the string "okay",
 // so we will ignore it for now.
 func reportDecodeError(reportURL, errMsg string) (bool, error) {
-	mapping := map[string]interface{} {
+	mapping := map[string]interface{}{
 		"error": errMsg,
 	}
 	marshalled, _ := json.Marshal(mapping)
@@ -83,9 +80,9 @@ func reportDecodeError(reportURL, errMsg string) (bool, error) {
 func lookup(lookupURL string) Result {
 	response, err := http.Get(BundleLookupURL(Configuration, lookupURL))
 	if err != nil || response.StatusCode != 200 {
-	  fmt.Print("error: ")
+		fmt.Print("error: ")
 		fmt.Println(err)
-    return Result{ ERR_NO_CONNECT_LCS, err.Error(), false, false, "" }
+		return Result{ERR_NO_CONNECT_LCS, err.Error(), false, false, ""}
 	}
 	decoder := json.NewDecoder(response.Body)
 	var result Result
@@ -94,10 +91,10 @@ func lookup(lookupURL string) Result {
 		fmt.Println(err)
 		reachedLCS, err2 := reportDecodeError(DecodeErrReportURL(Configuration), err.Error())
 		if reachedLCS {
-			return Result{ ERR_MALFORMED_LCS_RESPONSE, err2.Error(), false, false, "" }
+			return Result{ERR_MALFORMED_LCS_RESPONSE, err2.Error(), false, false, ""}
 		} else {
-      errMsg := "Could not reach the local cache server to report decode eror"
-			return Result{ ERR_NO_CONNECT_LCS, errMsg, false, false, "" }
+			errMsg := "Could not reach the local cache server to report decode eror"
+			return Result{ERR_NO_CONNECT_LCS, errMsg, false, false, ""}
 		}
 	}
 	return result
@@ -113,62 +110,65 @@ func requestNewBundle(lookupURL string, wasRewritten bool) error {
 		return err
 	}
 	req.Header.Set("Content-Type", "text/plain")
-  if wasRewritten {
-    req.Header.Set(REWRITTEN_HEADER, "true")
-  } else {
-    req.Header.Set(REWRITTEN_HEADER, "false")
-  }
+	if wasRewritten {
+		req.Header.Set(REWRITTEN_HEADER, "true")
+	} else {
+		req.Header.Set(REWRITTEN_HEADER, "false")
+	}
 	client := &http.Client{}
 	_, err2 := client.Do(req)
 	return err2
 }
 
 func execPleaseWait(URL string, w http.ResponseWriter, r *http.Request) {
-  body, isHTML := pleaseWait(URL)
-  if isHTML {
-    w.Header().Set("Content-Type", "text/html")
-  } else {
-    w.Header().Set("Content-Type", "text/plain")
-  }
-  w.Write(body)
+	body, isHTML := pleaseWait(URL)
+	if isHTML {
+		w.Header().Set("Content-Type", "text/html")
+	} else {
+		w.Header().Set("Content-Type", "text/plain")
+	}
+	w.Write(body)
 }
 
 // Handle incoming requests for bundles.
 // 1. Initiate bundle lookup process
 // 2. Initiate bundle creation process when no bundle exists anywhere
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
-  w = WriteProxyHeader(w)
+	w = WriteProxyHeader(w)
 	URL := r.URL.String()
-  wasRewritten := r.Header.Get(REWRITTEN_HEADER) == "true"
-  fmt.Printf("Got a request for %s\nRewritten: %v\n", URL, wasRewritten)
+	T, _ := i18n.Tfunc(os.Getenv("LANGUAGE"), "en-us")
+	str_ng := T("test1")
+	fmt.Println(str_ng)
+	wasRewritten := r.Header.Get(REWRITTEN_HEADER) == "true"
+	fmt.Printf("Got a request for %s\nRewritten: %v\n", URL, wasRewritten)
 	matched, err := regexp.MatchString(URL_REGEX, URL)
 	if !matched || err != nil {
 		fmt.Println("Invalid URL " + URL)
-    state := ErrorState{
-      "responseWriter": w, "request": r, "errMsg": "Malformed URL \"" + URL + "\"",
-    }
-    ErrorHandlers[ERR_MALFORMED_URL](state)
+		state := ErrorState{
+			"responseWriter": w, "request": r, "errMsg": "Malformed URL \"" + URL + "\"",
+		}
+		ErrorHandlers[ERR_MALFORMED_URL](state)
 		return
 	}
 	result := lookup(URL)
 	if result.ErrCode > 0 {
-    fmt.Printf("Got error from LCS: Error %v: %s\n", result.ErrCode, result.ErrMsg)
-    // Assuming the reason the response is malformed is because of the formation of the bundle,
-    // so we will request that a new bundle be created.
-    if result.ErrCode == ERR_MALFORMED_LCS_RESPONSE {
-      err = requestNewBundle(URL, wasRewritten)
-      fmt.Printf("Requested new bundle; Error: ")
-      fmt.Println(err)
-      if err != nil {
-        state := ErrorState{ "responseWriter": w, "request": r, "errMsg": err.Error() }
-        ErrorHandlers[ERR_FROM_LCS](state)
-      } else {
-        execPleaseWait(URL, w, r)
-      }
-    } else {
-      state := ErrorState{ "responseWriter": w, "request": r, "errMsg": result.ErrMsg }
-      ErrorHandlers[ERR_FROM_LCS](state)
-    }
+		fmt.Printf("Got error from LCS: Error %v: %s\n", result.ErrCode, result.ErrMsg)
+		// Assuming the reason the response is malformed is because of the formation of the bundle,
+		// so we will request that a new bundle be created.
+		if result.ErrCode == ERR_MALFORMED_LCS_RESPONSE {
+			err = requestNewBundle(URL, wasRewritten)
+			fmt.Printf("Requested new bundle; Error: ")
+			fmt.Println(err)
+			if err != nil {
+				state := ErrorState{"responseWriter": w, "request": r, "errMsg": err.Error()}
+				ErrorHandlers[ERR_FROM_LCS](state)
+			} else {
+				execPleaseWait(URL, w, r)
+			}
+		} else {
+			state := ErrorState{"responseWriter": w, "request": r, "errMsg": result.ErrMsg}
+			ErrorHandlers[ERR_FROM_LCS](state)
+		}
 	} else if result.Complete {
 		if result.Found {
 			w.Write([]byte(result.Bundle))
@@ -177,18 +177,21 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Error information from requestNewbundle")
 			fmt.Println(err)
 			if err != nil {
-        state := ErrorState{ "responseWriter": w, "request": r, "errMsg": err.Error() }
-        ErrorHandlers[ERR_FROM_LCS](state)
+				state := ErrorState{"responseWriter": w, "request": r, "errMsg": err.Error()}
+				ErrorHandlers[ERR_FROM_LCS](state)
 			} else {
-        execPleaseWait(URL, w, r)
+				execPleaseWait(URL, w, r)
 			}
 		}
 	} else {
-    execPleaseWait(URL, w, r)
+		execPleaseWait(URL, w, r)
 	}
 }
 
 func main() {
+	// Configure the i18n library to use the preferred language set in the LANGUAGE environement variable
+	language := os.Getenv("LANGUAGE")
+	i18n.MustLoadTranslationFile(path.Join("translations", language+".all.json"))
 	// Read an existing configuration file or have the user supply settings
 	conf, err := ReadConfigFile(CONFIG_FILE)
 	if err != nil {
@@ -201,6 +204,6 @@ func main() {
 	http.HandleFunc("/", proxyHandler)
 	fmt.Println("CeNo proxy server listening for HTTP requests at http://localhost" + Configuration.PortNumber)
 	if err = http.ListenAndServe(Configuration.PortNumber, nil); err != nil {
-    panic(err)
-  }
+		panic(err)
+	}
 }
--- ceno-node/errorhandling.go
+++ ceno-node/errorhandling.go
@@ -1,61 +1,59 @@
 package main
 
 import (
-  "github.com/nicksnyder/go-i18n/i18n"
-  "html/template"
-  "net/http"
-  "path"
-  "fmt"
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
 )
 
 const ( // CC errors
-  ERR_NO_CONFIG = 1100
-  ERR_MALFORMED_URL= 1101
-  ERR_NO_CONNECT_LCS = 1200
-  ERR_MALFORMED_LCS_RESPONSE = 1201
-  ERR_FROM_LCS = 1202
-  ERR_NO_CONNECT_RS = 1203
-  ERR_MISSING_VIEW = 1102
-  ERR_INVALID_ERROR = 100
+	ERR_NO_CONFIG              = 1100
+	ERR_MALFORMED_URL          = 1101
+	ERR_NO_CONNECT_LCS         = 1200
+	ERR_MALFORMED_LCS_RESPONSE = 1201
+	ERR_FROM_LCS               = 1202
+	ERR_NO_CONNECT_RS          = 1203
+	ERR_MISSING_VIEW           = 1102
+	ERR_INVALID_ERROR          = 100
 )
 
 const contactInfo = "ceno@equalit.ie"
 
 type ErrorSpec struct {
-  Url    string // Requested URL
-  Error  string // Name of error
-  Advice string // Suggestions for how to handle the bug
+	Url    string // Requested URL
+	Error  string // Name of error
+	Advice string // Suggestions for how to handle the bug
 }
 
 type ErrorCode uint32
 type ErrorState map[string]interface{}
 type ErrorHandler func(ErrorState) bool
 
-
 // Map error codes to the ids of explanations of errors that are localizable
-var ErrorAdvice = map[ErrorCode]string {
-  ERR_NO_CONFIG: "missing_config_err",
-  ERR_MALFORMED_URL: "malformed_url_err",
-  ERR_NO_CONNECT_LCS:"agent_communication_err",
-  ERR_MALFORMED_LCS_RESPONSE: "contact_devs_err",
-  ERR_FROM_LCS: "consult_readme_err",
-  ERR_NO_CONNECT_RS: "agent_communication_err",
-  ERR_MISSING_VIEW: "download_package_err",
-  ERR_INVALID_ERROR: "contact_devs_err",
+var ErrorAdvice = map[ErrorCode]string{
+	ERR_NO_CONFIG:              "missing_config_err",
+	ERR_MALFORMED_URL:          "malformed_url_err",
+	ERR_NO_CONNECT_LCS:         "agent_communication_err",
+	ERR_MALFORMED_LCS_RESPONSE: "contact_devs_err",
+	ERR_FROM_LCS:               "consult_readme_err",
+	ERR_NO_CONNECT_RS:          "agent_communication_err",
+	ERR_MISSING_VIEW:           "download_package_err",
+	ERR_INVALID_ERROR:          "contact_devs_err",
 }
 
 // An error handler for each of the errors that CC is expected to be responsible for.
 // Information about the state of the program during the time the error occurred, required
 // for the error to be handled, should be encoded into the ErrorState map.
-var ErrorHandlers = map[ErrorCode]func(ErrorState) bool {
-  ERR_NO_CONFIG: downloadConfigAndServeError,
-  ERR_MALFORMED_URL: serveError,
-  ERR_NO_CONNECT_LCS: serveError,
-  ERR_MALFORMED_LCS_RESPONSE: reportMalformationAndServeError,
-  ERR_FROM_LCS: handleLCSErrorReport,
-  ERR_NO_CONNECT_RS: serveError,
-  ERR_MISSING_VIEW: downloadViewsAndServeError,
-  ERR_INVALID_ERROR: serveError,
+var ErrorHandlers = map[ErrorCode]func(ErrorState) bool{
+	ERR_NO_CONFIG:              downloadConfigAndServeError,
+	ERR_MALFORMED_URL:          serveError,
+	ERR_NO_CONNECT_LCS:         serveError,
+	ERR_MALFORMED_LCS_RESPONSE: reportMalformationAndServeError,
+	ERR_FROM_LCS:               handleLCSErrorReport,
+	ERR_NO_CONNECT_RS:          serveError,
+	ERR_MISSING_VIEW:           downloadViewsAndServeError,
+	ERR_INVALID_ERROR:          serveError,
 }
 
 /********************
@@ -63,48 +61,53 @@ var ErrorHandlers = map[ErrorCode]func(ErrorState) bool {
  ********************/
 
 func serveError(state ErrorState) bool {
-  w := state["responseWriter"].(http.ResponseWriter)
-  r := state["request"].(*http.Request)
-  errMsg := state["errMsg"].(string)
-  ExecuteErrorPage(ERR_MALFORMED_URL, errMsg, w, r)
-  return true
+	w := state["responseWriter"].(http.ResponseWriter)
+	r := state["request"].(*http.Request)
+	errMsg := state["errMsg"].(string)
+	ExecuteErrorPage(ERR_MALFORMED_URL, errMsg, w, r)
+	return true
 }
 
 func downloadConfigAndServeError(state ErrorState) bool {
-  // temporary
-  return serveError(state)
+	// temporary
+	return serveError(state)
 }
 
 func reportMalformationAndServeError(state ErrorState) bool {
-  // temporary
-  return serveError(state)
+	// temporary
+	return serveError(state)
 }
 
 func handleLCSErrorReport(state ErrorState) bool {
-  // temporary
-  return serveError(state)
+	// temporary
+	return serveError(state)
 }
 
 func downloadViewsAndServeError(state ErrorState) bool {
-  // temporary
-  return serveError(state)
+	// temporary
+	return serveError(state)
 }
 
 // Execute the error template or produce a helpful plaintext response to explain
 // the error and provide pre-composed advice
 func ExecuteErrorPage(errorCode ErrorCode, errorMsg string, w http.ResponseWriter, r *http.Request) {
-  t, err := template.ParseFiles(path.Join(".", "views", "error.html"))
-  advice, foundErr := ErrorAdvice[errorCode]
-  if !foundErr {
-    ExecuteErrorPage(ERR_INVALID_ERROR, fmt.Sprintf("%v is not a recognized error code", errorCode), w, r)
-    return
-  }
-  errSpec := ErrorSpec{ r.URL.String(), errorMsg, advice }
-  if err != nil {
-    w.Header().Set("Content-Type", "text/plain")
-    advice2 := ErrorAdvice[ERR_MISSING_VIEW]
-    w.Write([]byte("Missing error.html view"))
-  } else {
-    t.Execute(w, errSpec)
-  }
+	t, err := template.ParseFiles(path.Join(".", "views", "error.html"))
+	advice, foundErr := ErrorAdvice[errorCode]
+	if !foundErr {
+    ExecuteErrorPage(
+      ERR_INVALID_ERROR, T("unrecognized_error_code", map[string]interface{} {
+        ErrorCode: errorCode
+      }
+    ), w, r)
+		return
+	}
+	errSpec := ErrorSpec{r.URL.String(), errorMsg, advice}
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(T("missing_view", map[string]interface{} {
+      View: "error.html"
+    }))
+	} else {
+		t.Execute(w, errSpec)
+	}
 }
