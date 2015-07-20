f601810ce64aac814a9a9fbf4944a7fe4bce6c78
Zack Mullaly <redwire@riseup.net>
2015-07-20 14:19:23 -0400

    Automate merging of translation files

--- ceno-client/build.sh
+++ ceno-client/build.sh
@@ -1,13 +1,16 @@
 #! /usr/bin/env sh
 
 echo "Formatting go files."
-
-for file in $(ls *.go); do
+for file in `ls *.go`; do
   go fmt $file;
 done
 
-echo "Installing dependencies"
-
+echo "Installing dependencies."
 go get -u github.com/nicksnyder/go-i18n/goi18n
 
-go build client.go errorhandling.go clientconfig.go && echo "Built client.go"
+echo "Merging localization files."
+cd translations
+$GOPATH/bin/goi18n *.json
+cd ..
+
+go build client.go errorhandling.go clientconfig.go && echo "Compiled client successfully."
