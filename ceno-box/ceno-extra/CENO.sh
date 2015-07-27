779503f84798f0fb56ee41b01a4b0dc28dbe6bfb
Marios Isaakidis <misaakidis@yahoo.gr>
2015-07-28 00:05:15 +0300

    CENO agents use the CENOLANG env variable for setting the locale; CENOBox identifies system's locale and whether one of the supported browser exists

--- ceno-box/ceno-extra/CENO.sh
+++ ceno-box/ceno-extra/CENO.sh
@@ -7,12 +7,22 @@ then
     exit 1
 fi
 
-# If $LANGUAGE environment variable is not set,
-# use "en-us" by default
-if [[ -z "$LANGUAGE" ]]
+# If $CENOLANG environment variable is not set,
+# use the $LANG or $LANGUAGE ones, or if neither
+# of the them is set, default to en-us
+if [[ -z "$CENOLANG" ]]
 then
-  export LANGUAGE=en-us
+  if [[ -n "$LANG" ]]
+  then
+    CENOLANG=$LANG
+  elif [[ -n "$LANGUAGE" ]]
+    CENOLANG=$LANGUAGE
+  else
+    CENOLANG=en-us
+  fi
 fi
+echo "CENO language set to" $CENOLANG
+export CENOLANG
 
 # Start the Freenet node
 ./run.sh start &> CENO.log
@@ -20,11 +30,17 @@ fi
 # Start CENOClient proxy
 cd ceno-client
 nohup ./client &> ../CENO.log &
-
-# Start a Firefox CENO session
 cd ..
-nohup firefox -no-remote -private-window -profile "ceno-firefox" &> /dev/null &
 
+# Open a browser window with the CENO profiles, including the plugin
+if command -v firefox >/dev/null 2>&1
+  then
+    firefox -no-remote -private-window -profile "ceno-firefox" &> /dev/null &
+  else
+    echo "None of the supported browsers is installed in your machine."
+    echo "Please install Chrome or Firefox and execute this script again."
+    exit 0
+fi
 
 echo "You are ready to use CENO."
-echo "Remember to configure your browser accordingly and always use a private/incognito session."
+echo "Remember that you are covered by CENO only when the CENO Router plugin is loaded in your browser."
