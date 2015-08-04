4c3a75b34089136b385be7a54bc09bf673d30918
Marios Isaakidis <misaakidis@yahoo.gr>
2015-08-04 19:12:07 +0200

    Plugin adds bridge as a friend and sends a freemail with its own node ref; Add bridge node ref as a resource; Add class for requesting and parsing reference files;

--- ceno-freenet/src/plugins/CENO/Backbone/CENOBackbone.java
+++ ceno-freenet/src/plugins/CENO/Backbone/CENOBackbone.java
@@ -1,18 +1,116 @@
 package plugins.CENO.Backbone;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import plugins.CENO.Version;
+import plugins.CENO.Client.CENOClient;
+import plugins.CENO.FreenetInterface.NodeInterface;
+import freenet.clients.http.ConnectionsToadlet.PeerAdditionReturnCodes;
+import freenet.io.comm.PeerParseException;
+import freenet.io.comm.ReferenceSignatureVerificationException;
+import freenet.node.DarknetPeerNode;
+import freenet.node.DarknetPeerNode.FRIEND_TRUST;
+import freenet.node.DarknetPeerNode.FRIEND_VISIBILITY;
+import freenet.node.FSParseException;
+import freenet.node.Node;
+import freenet.node.PeerNode;
 import freenet.pluginmanager.FredPlugin;
 import freenet.pluginmanager.FredPluginRealVersioned;
 import freenet.pluginmanager.FredPluginThreadless;
 import freenet.pluginmanager.FredPluginVersioned;
 import freenet.pluginmanager.PluginRespirator;
+import freenet.support.Fields;
+import freenet.support.Logger;
+import freenet.support.SimpleFieldSet;
 
+/**
+ * CENOBackbone plugin for Freenet
+ * 
+ * Its main purpose is to help route CENOBrdige packets
+ * faster to the area they should arrive to, based on the
+ * phenomenon of Small World Routing.
+ *
+ * Once the plugin is loaded, it will add as friend the
+ * bridge node (using the node reference in the resources)
+ * and send a Freemail to the bridge with its own reference,
+ * so that the bridge can add it back as a friend.
+ * Trust is set to HIGH and other friends of the Backbone
+ * node won't be able to see either the name or the node
+ * refernece of the Bridge node.
+ * 
+ * It is recommended that the Backbone nodes stay connected
+ * for as long as possible and are given a high
+ * bandwidth limit. The most straightforward way to spin
+ * a backbone router is to use the CENOBackboneBox.
+ * This plugin has a strong dependency on the WebOfTrust
+ * and Freemail official Freenet plugins.
+ *
+ */
 public class CENOBackbone implements FredPlugin, FredPluginVersioned, FredPluginRealVersioned, FredPluginThreadless {
 
 	private static final Version version = new Version(Version.PluginType.BACKBONE);
 
 	public static final String bridgeIdentityRequestURI = "USK@QfqLw7-BJpGGMnhnJQ3~KkCiciMAsoihBCtSqy6nNbY,-lG83h70XIJ03r4ckdNnsY4zIQ-J8qTqwzSBeIG5q3s,AQACAAE/WebOfTrust/0";
 	public static final String bridgeFreemail = "DEFLECTBridge@ih5ixq57yetjdbrspbtskdp6fjake4rdacziriiefnjkwlvhgw3a.freemail";
+
+	public static final String backboneIdentityInsertURI = "";
+	public static final String backboneFreemail = "";
+	
+	public static Node node;
+	private NodeRefHelper nodeRefHelper;
+	public static NodeInterface nodeInterface;
+	
+	@Override
+	public void runPlugin(PluginRespirator pr) {
+		node = pr.getNode();
+		nodeRefHelper = new NodeRefHelper(node);
+		PeerAdditionReturnCodes addBridgeResult = addFriendBridge();
+		if (addBridgeResult == PeerAdditionReturnCodes.ALREADY_IN_REFERENCE || addBridgeResult == PeerAdditionReturnCodes.OK) {
+			Logger.normal(this, "Successfully added the node in bridgeref.txt resource file as friend.");
+		} else {
+			Logger.error(this, "Error while adding Bridge node as a friend, will terminate Backbone plugin...");
+			terminate();
+		}
+		nodeInterface = new NodeInterface(pr.getNode(), pr);
+		nodeInterface.sendFreemail(CENOClient.clientFreemail, new String[]{bridgeFreemail}, "addFriend", nodeRefHelper.getNodeRef(), "CENO");
+	}
+	
+	private PeerAdditionReturnCodes addFriendBridge() {
+		SimpleFieldSet bridgeNodeFS;
+		try {
+			bridgeNodeFS = nodeRefHelper.getBridgeNodeRefFS();
+		} catch (IOException e) {
+			Logger.error(this, "IO Exception while parsing bridge reference resource file");
+			return PeerAdditionReturnCodes.INTERNAL_ERROR;
+		}
+		PeerNode pn;
+		try {
+			if(node.isOpennetEnabled()) {
+				pn = node.createNewOpennetNode(bridgeNodeFS);
+			} else {
+				pn = node.createNewDarknetNode(bridgeNodeFS, FRIEND_TRUST.HIGH, FRIEND_VISIBILITY.NO);
+				((DarknetPeerNode)pn).setPrivateDarknetCommentNote("Bridge");
+			}
+		} catch (FSParseException e1) {
+			return PeerAdditionReturnCodes.CANT_PARSE;
+		} catch (PeerParseException e1) {
+			return PeerAdditionReturnCodes.CANT_PARSE;
+		} catch (ReferenceSignatureVerificationException e1){
+			return PeerAdditionReturnCodes.INVALID_SIGNATURE;
+		} catch (Throwable t) {
+            Logger.error(this, "Internal error adding reference :" + t.getMessage(), t);
+			return PeerAdditionReturnCodes.INTERNAL_ERROR;
+		}
+		if(Arrays.equals(pn.getPubKeyHash(), node.getDarknetPubKeyHash())) {
+			Logger.warning(this, "The bridge  node reference file belongs to this node.");
+			return PeerAdditionReturnCodes.TRY_TO_ADD_SELF;
+		}
+		if(!this.node.addPeerConnection(pn)) {
+			return PeerAdditionReturnCodes.ALREADY_IN_REFERENCE;
+		}
+		return PeerAdditionReturnCodes.OK;
+	}
 
 	public String getVersion() {
 		return version.getVersion();
@@ -24,12 +122,6 @@ public class CENOBackbone implements FredPlugin, FredPluginVersioned, FredPlugin
 
 	@Override
 	public void terminate() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void runPlugin(PluginRespirator pr) {
 		// TODO Auto-generated method stub
 
 	}
---
+++ ceno-freenet/src/plugins/CENO/Backbone/NodeRefHelper.java
@@ -0,0 +1,54 @@
+package plugins.CENO.Backbone;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import freenet.node.Node;
+import freenet.support.Fields;
+import freenet.support.SimpleFieldSet;
+
+public class NodeRefHelper {
+	private Node node;
+	
+	public NodeRefHelper (Node node) {
+		this.node = node;
+	}
+
+	public String getNodeRef() {
+		SimpleFieldSet fs = getNodeRefFS();
+		return fs.toOrderedStringWithBase64();
+	}
+
+	public SimpleFieldSet getNodeRefFS() {
+		return node.exportDarknetPublicFieldSet();
+	}
+
+	public String getBridgeNodeRef() throws FileNotFoundException, IOException {
+		InputStream is = CENOBackbone.class.getResourceAsStream("bridgeref.txt");
+		if (is == null) {
+			throw new FileNotFoundException();
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		String line = "";
+		StringBuilder bridgeRef = new StringBuilder();
+		while ((line = br.readLine()) != null) {
+			bridgeRef.append(line);
+		}
+		
+		return bridgeRef.toString();
+	}
+
+	public SimpleFieldSet getBridgeNodeRefFS() throws IOException {
+		SimpleFieldSet fs;
+		String bridgeRef = Fields.trimLines(getBridgeNodeRef());
+		fs = new SimpleFieldSet(bridgeRef, false, true, true);
+		if(!fs.getEndMarker().endsWith("End")) {
+			throw new IOException("Trying to add noderef with end marker \""+fs.getEndMarker()+"\"");
+		}
+		fs.setEndMarker("End"); // It's always End ; the regex above doesn't always grok this
+		return fs;
+	}
+}
