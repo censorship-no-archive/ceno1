5ae31ca9a6adbcc349cd055d235985a1e5cef713
Marios Isaakidis <misaakidis@yahoo.gr>
2015-08-25 13:32:33 +0300

    Replace CENOReqFlags array with an EnumSet

--- ceno-freenet/src/plugins/CENO/CENORequest.java
+++ ceno-freenet/src/plugins/CENO/CENORequest.java
@@ -1,16 +1,22 @@
 package plugins.CENO;
 
+import java.util.EnumSet;
+
 import plugins.CENO.FreenetInterface.FreemailAPI.Freemail;
 
 public class CENORequest extends Freemail {
 
-	private CENOReqFlags[] reqFlags;
+	public enum CENOReqFlags {
+		X_Ceno_Rewritten
+	}
+
+	private EnumSet<CENOReqFlags> reqFlags;
 
 	public CENORequest(Freemail freemail) {
 		super(freemail.getFreemailFrom(), freemail.getFreemailTo(), freemail.getSubject(), freemail.getBody());
 	}
 
-	public CENORequest(Freemail freemail, CENOReqFlags[] reqFlags) {
+	public CENORequest(Freemail freemail, EnumSet<CENOReqFlags> reqFlags) {
 		this(freemail);
 		this.reqFlags = reqFlags;
 	}
@@ -19,16 +25,12 @@ public class CENORequest extends Freemail {
 		super(freemailFrom, freemailTo, subject, body);
 	}
 
-	public CENORequest(String freemailFrom, String[] freemailTo, String subject, String body, CENOReqFlags[] reqFlags) {
+	public CENORequest(String freemailFrom, String[] freemailTo, String subject, String body, EnumSet<CENOReqFlags> reqFlags) {
 		this(freemailFrom, freemailTo, subject, body);
 		this.reqFlags = reqFlags;
 	}
 
-	public CENOReqFlags[] getReqFlags() {
+	public EnumSet<CENOReqFlags> getReqFlags() {
 		return reqFlags;
-	}
-	
-	public enum CENOReqFlags {
-		X_Ceno_Rewritten
 	}
 }
