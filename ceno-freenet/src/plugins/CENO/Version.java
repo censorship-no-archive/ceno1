6e516d665ec36a4c3538100f921a877032fb8b34
Marios Isaakidis <misaakidis@yahoo.gr>
2015-09-10 23:16:02 +0300

    Release CENO v0.3.0 stable

--- ceno-freenet/src/plugins/CENO/Version.java
+++ ceno-freenet/src/plugins/CENO/Version.java
@@ -4,24 +4,24 @@ import freenet.pluginmanager.FredPluginRealVersioned;
 import freenet.pluginmanager.FredPluginVersioned;
 
 public class Version implements FredPluginVersioned, FredPluginRealVersioned {
-	
+
 	// Versions of the plugins, in human-readable and "real" format
-	public static final String CLIENT_VERSION = "0.3.0-rc2";
-	public static final int CLIENT_REAL_VERSION = 6;
-	
-	public static final String BRIDGE_VERSION = "0.3.0-rc2";
-	public static final int BRIDGE_REAL_VERSION = 4;
-	
+	public static final String CLIENT_VERSION = "0.3.0";
+	public static final int CLIENT_REAL_VERSION = 7;
+
+	public static final String BRIDGE_VERSION = "0.3.0";
+	public static final int BRIDGE_REAL_VERSION = 5;
+
 	public static final String BACKBONE_VERSION = "0.1.0";
 	public static final int BACKBONE_REAL_VERSION = 1;
-	
+
 	/** Revision number of Version.java as read from CVS */
 	public static final String cvsRevision = "@custom@";
-	
+
 	public enum PluginType { CLIENT, BRIDGE, BACKBONE };
-	
+
 	private PluginType pluginType;
-	
+
 	public Version(PluginType pluginType) {
 		this.pluginType = pluginType;
 	}
@@ -49,5 +49,5 @@ public class Version implements FredPluginVersioned, FredPluginRealVersioned {
 		}
 		return "Unknown";
 	}
-	
+
 }
