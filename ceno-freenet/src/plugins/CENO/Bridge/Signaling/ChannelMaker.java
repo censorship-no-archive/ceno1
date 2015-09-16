fad03ee86c5aecaaa5732df80d7d9ddc1802edfd
Marios Isaakidis <misaakidis@yahoo.gr>
2015-09-16 19:00:15 +0300

    ChannelMaker publishes a freesite that includes a puzzle question at USK@<bridge-key>/CENO-signaler/version/default.html

--- ceno-freenet/src/plugins/CENO/Bridge/Signaling/ChannelMaker.java
+++ ceno-freenet/src/plugins/CENO/Bridge/Signaling/ChannelMaker.java
@@ -1,23 +1,38 @@
 package plugins.CENO.Bridge.Signaling;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 
+import plugins.CENO.CENOErrCode;
+import plugins.CENO.CENOException;
 import plugins.CENO.Bridge.CENOBridge;
+import freenet.client.InsertException;
+import freenet.client.async.BaseClientPutter;
+import freenet.client.async.ClientContext;
+import freenet.client.async.ClientPutCallback;
 import freenet.keys.FreenetURI;
+import freenet.node.RequestClient;
 import freenet.support.SimpleFieldSet;
+import freenet.support.api.Bucket;
+import freenet.support.io.ResumeFailedException;
 
 public class ChannelMaker {
 	private String bridgeInsertURI;
 	private String privateRSA;
 	private String publicRSA;
 	
-	public ChannelMaker(String insertURI, String[] RSA) throws MalformedURLException {
+	public ChannelMaker(String insertURI, String[] RSA) throws CENOException {
 		this.bridgeInsertURI = insertURI;
 		this.privateRSA = RSA[0];
 		this.publicRSA = RSA[1];
 		
+		try {
 		ChannelAnnouncer channelAnnouncer = new ChannelAnnouncer(bridgeInsertURI, publicRSA, generatePuzzle().getQuestion());
 		channelAnnouncer.doAnnounce();
+		} catch (IOException e) {
+			throw new CENOException(CENOErrCode.RR, "IOException in announcement channel creation");
+		} catch (InsertException e) {
+			throw new CENOException(CENOErrCode.RR, "Could not insert announcement channel");
+		}
 	}
 	
 	private Puzzle generatePuzzle() {
@@ -37,14 +52,15 @@ public class ChannelMaker {
 			this.puzzleQuestion = puzzleQuestion;
 		}
 		
-		public void doAnnounce() throws MalformedURLException {
+		public void doAnnounce() throws IOException, InsertException {
 			SimpleFieldSet sfs = new SimpleFieldSet(false, true);
 			sfs.putOverwrite("publicRSA", publicRSA);
 			sfs.putOverwrite("question", puzzleQuestion);
 			
 			FreenetURI insertURIconfig = new FreenetURI(bridgeInsertURI);
 			FreenetURI announcementURI = new FreenetURI("USK", CENOBridge.announcerPath, insertURIconfig.getRoutingKey(), insertURIconfig.getCryptoKey(), insertURIconfig.getExtra());
-			//TODO publish channel announcement freesite
+			
+			CENOBridge.nodeInterface.insertFreesite(announcementURI, "default.html", sfs.toOrderedString(), new AnnouncementInsertionCB());
 		}
 	}
 	
@@ -73,5 +89,38 @@ public class ChannelMaker {
 		public String getAnswer() {
 			return answer;
 		}
+	}
+	
+	private class AnnouncementInsertionCB implements ClientPutCallback {
+
+		@Override
+		public void onResume(ClientContext context) throws ResumeFailedException {
+		}
+
+		@Override
+		public RequestClient getRequestClient() {
+			return CENOBridge.nodeInterface.getRequestClient();
+		}
+
+		@Override
+		public void onGeneratedURI(FreenetURI uri, BaseClientPutter state) {
+		}
+
+		@Override
+		public void onGeneratedMetadata(Bucket metadata, BaseClientPutter state) {
+		}
+
+		@Override
+		public void onFetchable(BaseClientPutter state) {
+		}
+
+		@Override
+		public void onSuccess(BaseClientPutter state) {
+		}
+
+		@Override
+		public void onFailure(InsertException e, BaseClientPutter state) {			
+		}
+		
 	}
 }
