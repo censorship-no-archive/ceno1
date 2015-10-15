0508e2095741b709d0194be5b527b78e800044a5
Zack Mullaly <redwire@riseup.net>
2015-10-15 11:37:11 -0400

    Clean up build script

--- ceno-client/build.sh
+++ ceno-client/build.sh
@@ -1,7 +1,13 @@
 #! /usr/bin/env sh
 
+SOURCE_FILES="src/client.go \
+    src/errorhandling.go \
+    src/clientconfig.go \
+    src/portal.go \
+    src/articles.go"
+
 echo "Formatting go files."
-for file in `ls *.go`; do
+for file in `ls src/*.go`; do
   go fmt $file;
 done
 
@@ -13,4 +19,4 @@ cd translations
 $GOPATH/bin/goi18n *.all.json *.untranslated.json
 cd ..
 
-go build client.go errorhandling.go clientconfig.go && echo "Compiled client successfully."
+go build $SOURCE_FILES && echo "Compiled client successfully."
