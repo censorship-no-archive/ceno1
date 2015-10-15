7f2ac74a0102c14d0acd7ba31221ad7f06f7a4e1
Zack Mullaly <redwire@riseup.net>
2015-10-15 12:56:52 -0400

    Have build script refer to new config.go name

--- ceno-client/build.sh
+++ ceno-client/build.sh
@@ -2,10 +2,11 @@
 
 SOURCE_FILES="src/client.go \
     src/errorhandling.go \
-    src/clientconfig.go \
+    src/config.go \
     src/data.go \
     src/portal.go \
-    src/articles.go"
+    src/articles.go \
+    src/resources.go"
 
 echo "Formatting go files."
 for file in `ls src/*.go`; do
@@ -13,6 +14,7 @@ for file in `ls src/*.go`; do
 done
 
 echo "Installing dependencies."
+echo "goi18n"
 go get -u github.com/nicksnyder/go-i18n/goi18n
 
 echo "Merging localization files."
