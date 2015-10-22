43b77a2f8f3b706b50ff9263419bdd3e80ceddc9
Zack Mullaly <redwire@riseup.net>
2015-10-22 12:04:15 -0400

    Tools to extract sources from zinapp and to issue follow requests

---
+++ ceno-reader/tools/zinapp-extract.py
@@ -0,0 +1,23 @@
+import sys
+from bs4 import BeautifulSoup as bs
+
+if len(sys.argv) != 2:
+    print 'Usage: python {0} <sources.html>'.format(sys.argv[0])
+    sys.exit(1)
+
+
+html = open(sys.argv[1]).read()
+soup = bs(html, 'html.parser')
+table = [t for t in soup.find_all('table') if t.get('id', '') == 'discovery'][0]
+rows = table.tbody.find_all('tr')
+
+output = open('sources.list', 'w')
+for row in rows:
+    _, _, _, _, feed_url, feed_type = row.find_all('td')
+    url = feed_url.a.text
+    _type = feed_type.text
+    if _type.lower() == 'html':
+        output.write(url + '\n')
+        print url
+
+output.close()
