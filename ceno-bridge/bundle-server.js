742155fe52bb91b2c247b1de06096351c03a62ee
Zack Mullaly <redwire@riseup.net>
2015-11-11 14:42:28 -0500

    Touched up comments in the bundle server

--- ceno-bridge/bundle-server.js
+++ ceno-bridge/bundle-server.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
-
 var bsHandlerMaker = require('./bshandler');
 var _t = require('./translations');
 
 const configFile = path.join('config', 'node.json');
 
+// A default configuration to run the bundle server in case the expected configuration file
+// cannot be found or loaded for some reason.
 var defaultConfig = {
   port: 3094,
   requestReceiver: 'http://localhost:3093',
@@ -14,6 +15,7 @@ var defaultConfig = {
   userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36';
 };
 
+// Apply the default configuration for now.
 var config = defaultConfig;
 
 try {
