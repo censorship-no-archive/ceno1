65713fd487c333803e9fdede0406a0dbe81e89df
Zack Mullaly <redwire@riseup.net>
2015-11-16 12:46:24 -0500

    Added a task for concatenating javascripts

--- ceno-client/gulpfile.js
+++ ceno-client/gulpfile.js
@@ -1,13 +1,22 @@
 'use strict';
 
+let fs = require('fs');
 let gulp = require('gulp');
 let jshint = require('gulp-jshint');
 let stylish = require('jshint-stylish');
+let concat = require('gulp-concat');
 
 gulp.task('lintjs', () => {
   return gulp.src('./portal/js/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('concatjs', () => {
+  let jsFiles = fs.readdirSync('./portal/js').map((filename) => `./portal/js/${filename}`);
+  return gulp.src(jsFiles)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./static/javascript/'));
 });
 
 gulp.task('build', ['lintjs']);
