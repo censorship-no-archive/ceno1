0ba84f50cca3ede20cae322d805ec2387d657d1c
Zack Mullaly <redwire@riseup.net>
2015-11-16 14:45:21 -0500

    Have the URL lookup form redirect the user to a properly-formatted direct lookup route handled by the CC

--- ceno-client/portal/js/index.js
+++ ceno-client/portal/js/index.js
@@ -2,16 +2,27 @@
 'use strict';
 
 /**
+ * Produce a URL that can be redirected to in order to have the CENO client do a lookup
+ * for a given site.
+ * @param {string} siteUrl - The URL of the site the user wants to visit (e.g. https://google.ca/)
+ * @return {string} the URL to request from the CENO client
+ */
+function lookupUrl(siteUrl) {
+  return `http://localhost:3090/lookup?url=${btoa(siteUrl)}`;
+}
+
+/**
  * When the user submits the form for requesting a site from the home page,
  * we want to intercept that request and base64-encode the URL they entered so that the
  * request integrates seamlessly with the existing direct lookup functionality.
  */
 function encodeLookupUrl(e) {
   e.preventDefault();
+  console.log('Got lookup request');
   let urlInput = document.getElementById('indexUrlSearch');
-  let newUrl = btoa(urlInput.value);
-  urlInput.value = newUrl;
-  document.lookupForm.submit();
+  let newUrl = lookupUrl(urlInput.value);
+  console.log('New url is', newUrl);
+  window.location.href = newUrl;
 }
 
 // Attach the handler above to the form if we're on the index page.
