39c40ea45c8837b1d54e408df40a3ad7c798dd97
Marios Isaakidis <misaakidis@yahoo.gr>
2015-11-17 18:21:58 -0500

    Refactor BundleInserter; Add a queue to limit max concurrent insertions to 5

--- ceno-freenet/src/plugins/CENO/Bridge/RequestReceiver.java
+++ ceno-freenet/src/plugins/CENO/Bridge/RequestReceiver.java
@@ -121,11 +121,11 @@ public class RequestReceiver {
 										continue;
 									}
 									// Pass the request to the BundleInserter agent
-									if (!BundleInserter.shouldInsert(urlRequested)) {
+									if (!BundleInserter.getInstance().shouldInsert(urlRequested)) {
 										Logger.normal(this, "Bundle for URL: " + urlRequested + " is not stale yet, will not re-insert");
 										continue;
 									}
-									BundleInserter.insertBundle(urlRequested);
+									BundleInserter.getInstance().insertBundle(urlRequested);
 								} catch (IOException e) {
 									Logger.error(this, "I/O exception while requesting/inserting the bundle for URL: " + urlRequested + 
 											" Error: " + e.getMessage());
