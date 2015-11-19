671a06860843cc452e91d854a8845c792e34c3fc
Zack Mullaly <redwire@riseup.net>
2015-11-19 13:01:29 -0500

    Created locale files for text to appear on the portal along with a sample French translation and a gulp task to merge the translations

--- ceno-client/gulpfile.js
+++ ceno-client/gulpfile.js
@@ -7,36 +7,43 @@ let stylish = require('jshint-stylish');
 let concat = require('gulp-concat');
 let csslint = require('gulp-csslint');
 let babel = require('gulp-babel');
+let jsoncombine = require('gulp-jsoncombine');
 
-gulp.task('lintjs', () => {
-  return gulp.src('./portal/js/*.js')
+gulp.task('lintjs', () =>
+  gulp.src('./portal/js/*.js')
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter(stylish));
-});
+    .pipe(jshint.reporter(stylish))
+);
 
-gulp.task('concatjs', () => {
-  return gulp.src('./portal/js/*.js')
+gulp.task('concatjs', () =>
+  gulp.src('./portal/js/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./static/javascript/'));
-});
+    .pipe(gulp.dest('./static/javascript/'))
+);
 
-gulp.task('concatcss', () => {
-  return gulp.src('./portal/css/*.css')
+gulp.task('concatcss', () =>
+  gulp.src('./portal/css/*.css')
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('./static/stylesheets/'));
-});
+    .pipe(gulp.dest('./static/stylesheets/'))
+);
 
-gulp.task('copyhtml', () => {
-  return gulp.src('./portal/html/*.html')
-    .pipe(gulp.dest('./views/'));
-});
+gulp.task('copyhtml', () =>
+  gulp.src('./portal/html/*.html')
+    .pipe(gulp.dest('./views/'))
+);
+
+gulp.task('translations', () =>
+  gulp.src('./portal/locale/*.json')
+    .pipe(jsoncombine('all.json', (data) => new Buffer(JSON.stringify(data))))
+    .pipe(gulp.dest('./locale/'))
+);
 
 // Shorthand tasks for applying changes to single resources
 gulp.task('js', ['lintjs', 'concatjs']);
 gulp.task('css', ['concatcss']);
 gulp.task('html', ['copyhtml']);
 
-gulp.task('build', ['lintjs', 'concatjs', 'concatcss', 'copyhtml']);
+gulp.task('build', ['lintjs', 'concatjs', 'concatcss', 'copyhtml', 'translations']);
