a682288e4fd17f24aa945fbef4d43764763e4c8c
Zack Mullaly <redwire@riseup.net>
2015-11-27 12:38:24 -0500

    A small web server to simulate the LCS responding to /status

---
+++ ceno-client/test/statusbi.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let createServer = require('http').createServer;
+
+function handler(request, response) {
+  console.log('Got a request for connectivity status');
+  response.setHeader('Content-Type', 'application/json');
+  response.write(JSON.stringify({
+    status: 'okay',
+    message: 'All systems green!'
+  }));
+  response.end();
+}
+
+createServer(handler).listen(3091);
+console.log('Listening on http://localhost:3091');
