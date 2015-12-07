36383c8a090c9b0b16c8221376b7df7de83ae1b0
Marios Isaakidis <misaakidis@yahoo.gr>
2015-12-07 15:30:11 -0500

    Bring back Firefox prefs.js

---
+++ browser-profiles/firefox/prefs.js
@@ -0,0 +1,16 @@
+user_pref("network.proxy.backup.ftp", "");
+user_pref("network.proxy.backup.ftp_port", 0);
+user_pref("network.proxy.backup.socks", "");
+user_pref("network.proxy.backup.socks_port", 0);
+user_pref("network.proxy.backup.ssl", "");
+user_pref("network.proxy.backup.ssl_port", 0);
+user_pref("network.proxy.ftp", "127.0.0.1");
+user_pref("network.proxy.ftp_port", 3090);
+user_pref("network.proxy.http", "127.0.0.1");
+user_pref("network.proxy.http_port", 3090);
+user_pref("network.proxy.share_proxy_settings", true);
+user_pref("network.proxy.socks", "127.0.0.1");
+user_pref("network.proxy.socks_port", 3090);
+user_pref("network.proxy.ssl", "127.0.0.1");
+user_pref("network.proxy.ssl_port", 3090);
+user_pref("network.proxy.type", 1);
