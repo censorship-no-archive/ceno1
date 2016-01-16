ea8173ea1722530a918ab3a35d61a14203d88fc7
Marios Isaakidis <misaakidis@yahoo.gr>
2016-01-16 13:00:38 +0000

    Bundle Inserter won't request from Bundle Server URLs that are causing BS to panic

--- ceno-freenet/src/plugins/CENO/Bridge/BundlerInterface/Bundle.java
+++ ceno-freenet/src/plugins/CENO/Bridge/BundlerInterface/Bundle.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 import net.minidev.json.JSONObject;
 import net.minidev.json.JSONValue;
@@ -15,10 +17,10 @@ import plugins.CENO.Common.URLtoUSKTools;
 public class Bundle {
 	private String uri;
 	private String content;
+	private static Set<String> breakingUrls = new HashSet<String>();
 
 	public Bundle(String URI) {
 		this.uri = URI;
-		content = "Bundle for URI " + URI;
 	}
 
 	public String getContent() {
@@ -53,24 +55,31 @@ public class Bundle {
 	}
 
 	private void doRequest() throws IOException, ParseException {
+		if (breakingUrls.contains(uri)) {
+			throw new IOException("Will not request URL " + uri + "from Bundle Server for robustness reasons");
+		}
 		uri = URLtoUSKTools.b64EncSafe("http://" + URLtoUSKTools.validateURL(uri));
 		URL url = new URL("http", "127.0.0.1", CENOBridge.bundleServerPort, "/?url=" + uri);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		String line;
-		StringBuffer response = new StringBuffer(); 
-		while ((line = in.readLine()) != null) {
-			response.append(line);
-			response.append('\r');
+		StringBuffer response = new StringBuffer();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				response.append(line);
+				response.append('\r');
+			}
+			in.close();
+		} catch (IOException e) {
+			breakingUrls.add(uri);
+			throw new IOException("Request for URL " + uri + " killed Bundle Server, won't try to fetch again");
 		}
-		in.close();
 
 		JSONObject jsonResponse = (JSONObject) JSONValue.parseWithException(response.toString());
 		if (jsonResponse.containsKey("error")) {
 			throw new IOException("Response from bundle server included error: " + jsonResponse.get("error"));
 		}
-		
+
 		content = (String) jsonResponse.get("bundle");
 		return;
 	}
