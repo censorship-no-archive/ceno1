5a36e2e3777e8fb52c450df6abe04bbf1d056051
Veggie Monster <vmonmoonshine@gmail.com>
2016-04-13 15:57:15 -0400

    add InsertFeedListOnly to ceno reader

--- ceno-reader/src/reader.go
+++ ceno-reader/src/reader.go
@@ -18,6 +18,7 @@ import (
 )
 
 var insertionPause int
+var insertFeedListOnly bool
 
 /**
  * Log the current time and a message
@@ -175,6 +176,10 @@ func followFeeds(requests chan SaveFeedRequest) {
 			}
 			request.W.Write([]byte(T("req_handle_success_rdr")))
 		}
+		if insertFeedListOnly {
+			log("We are asked to only insert the feed list, skiping polling feeds.")
+			continue
+		}
 		if feedInfo.Charset == "" {
 			go pollFeed(feedInfo.Url, nil)
 		} else {
@@ -270,6 +275,12 @@ func insertHandler(w http.ResponseWriter, r *http.Request) {
 		log(writeFeedsErr)
 		//return none the less write the feedlist in freenet
 	}
+
+	if insertFeedListOnly {
+		log("We are asked to only insert the feed list, not inserting the item lists.")
+		return
+	}
+
 	for _, feed := range feeds {
 		items, itemsError := GetItems(DBConnection, feed.Url)
 		if itemsError != nil {
@@ -417,6 +428,7 @@ func main() {
 		Configuration = conf
 	}
 	insertionPause = Configuration.InsertionPause
+	insertFeedListOnly = Configuration.InsertFeedListOnly
 	// Establish a connection to the database
 	var dbErr error
 	DBConnection, dbErr = InitDBConnection(DB_FILENAME)
